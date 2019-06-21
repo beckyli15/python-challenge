3618ca2bb9df9d85fb7a7a26f9a42dc6764c1c03
beckyli15 <50608335+beckyli15@users.noreply.github.com>
2019-06-20 17:53:18 -0700

    PyPoll final

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,7 @@ vote_per_candidate=[]
 percent_vote=[]
 candidate_votes=0
 organized_candidate_list=[]
+final_list=[]
 
 csvpath=os.path.join("election_data.csv")
 with open(csvpath,newline="") as csvfile:
@@ -18,25 +19,36 @@ with open(csvpath,newline="") as csvfile:
         total_vote=total_vote+1
         candidate_list.append(row[2])
     for candidate in set(candidate_list):
-        unique_candidate.append(candidate) #unique_candidate=[khan] 
         # print(candidate)
         candidate_votes = candidate_list.count(candidate) #2218231
         candidate_vote_percentage = ((candidate_votes/total_vote)*100)#63
-        unique_candidate.append('{:.2f}%'.format(candidate_vote_percentage)) #unique_candidate=[khan,63]
         unique_candidate.append(candidate_votes) #unique_candidate=[khan,63,2218231]
-        print(unique_candidate)
-        
-        
-        candidate.loc[candidate['candidate_votes'].idxmax(candidate_votes)]
-    
-        # organized_candidate_list.append(unique_candidate) #organized_candidate_list = [[khan, 63, 2218231]]
+        unique_candidate.append('{:.2f}%'.format(candidate_vote_percentage)) #unique_candidate=[khan,63]
+        unique_candidate.append(candidate) #unique_candidate=[khan] 
+        # print(unique_candidate)
+        # unique_candidate=[]
+        organized_candidate_list.append(unique_candidate) #organized_candidate_list = [[khan, 63, 2218231]]
+        unique_candidate=[]
+       
+        # candidate.loc[candidate['candidate_votes'].idxmax(candidate_votes)]
+
+textfile= open("main.txt","w")
+for candy in organized_candidate_list:
+    per_string = str(candy[1])
+    vote_string = str(candy[0])
+    line_entry=candy[2] + ": " + per_string + "(" + vote_string + ")\n"
+    print(candy[2], ": ", candy[1], "(", candy[0], ")")
+    textfile.writelines( line_entry )
+
+
+winner_line = "Winner: " + max(organized_candidate_list)[2]
+print("-------------------------")
+print(winner_line)
+print("-------------------------")
 
 
 
-
-    # print(organized_candidate_list)
-# for candy in organized_candidate_list:
-#     print(candy[0], ": ", candy[1], "(", candy[2], ")")
+textfile.writelines(winner_line)
 
     # print(total_vote)
     # print(unique_candidate)
